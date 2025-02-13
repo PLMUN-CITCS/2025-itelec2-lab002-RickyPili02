9a7100f8373cbf7a57a659747be3b637687c6683
RickyPili02 <>
2025-02-13 18:39:08 +0800

    Last Sync: 2025-02-13 10:39:08 (Mobile)

---
+++ problem-02.py
@@ -0,0 +1,16 @@
+# Ricky Pili
+# ITELEC2
+# Problem Set 01 - Problem 02
+# Square the Number Program
+
+def main():
+    print("Square the Number Program")  # Introduce the program
+
+    # Obtain user input
+    number = int(input("Enter a number: "))
+
+    # Compute and display the square
+    print(f"The square of {number} is {(number * number):.2f}")
+
+if __name__ == "__main__":
+    main()
