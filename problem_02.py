0825d5b46f7be8e8d7f4660eca794414e4195287
RickyPili02 <>
2025-02-13 18:28:24 +0800

    Last Sync: 2025-02-13 10:28:24 (Mobile)

--- problem_02.py
+++ problem_02.py
@@ -1,10 +1,16 @@
-# YOUR NAME
+# Ricky Pili
 # ITELEC2
 # Problem Set 01 - Problem 02
 # Square the Number Program
 
 def main():
-    pass # replace this line with all of your code
+    print("Square the Number Program")  # Introduce the program
+
+    # Obtain user input
+    number = int(input("Enter a number: "))
+
+    # Compute and display the square
+    print(f"The square of {number} is {(number * number):.2f}")
 
 if __name__ == "__main__":
     main()
