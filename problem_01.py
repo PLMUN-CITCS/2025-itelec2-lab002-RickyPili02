099e888cfbe70822696b7090c03611c4fee5066a
RickyPili02 <>
2025-02-13 19:09:05 +0800

    Last Sync: 2025-02-13 11:09:05 (Mobile)

--- problem_01.py
+++ problem_01.py
@@ -4,17 +4,14 @@
 # Simple Calculator Program
 
 def main():
-    print("Simple Calculator Program")  # Introduce the program
-
-    # Obtain user input
+    print("Simple Calculator Program")
     num1 = int(input("Enter 1st number: "))
     num2 = int(input("Enter 2nd number: "))
 
-    # Perform arithmetic operations
     print(f"The sum is {num1 + num2}")
     print(f"The difference is {num1 - num2}")
     print(f"The product is {num1 * num2}")
-    print(f"The quotient is {(num1 / num2):.2f}")
+    print(f"The quotient is {round(num1 / num2, 2):.2f}") 
 
 if __name__ == "__main__":
-    main()
+    main(
