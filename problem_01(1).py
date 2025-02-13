9871355df94e2c3e80daf96aade45047be173488
RickyPili02 <>
2025-02-13 18:25:17 +0800

    Last Sync: 2025-02-13 10:25:17 (Mobile)

---
+++ problem_01(1).py
@@ -0,0 +1,20 @@
+# Ricky Pili
+# ITELEC2
+# Problem Set 01 - Problem 01
+# Simple Calculator Program
+
+def main():
+    print("Simple Calculator Program")  # Introduce the program
+
+    # Obtain user input
+    num1 = int(input("Enter 1st number: "))
+    num2 = int(input("Enter 2nd number: "))
+
+    # Perform arithmetic operations
+    print(f"The sum is {num1 + num2}")
+    print(f"The difference is {num1 - num2}")
+    print(f"The product is {num1 * num2}")
+    print(f"The quotient is {(num1 / num2):.2f}")
+
+if __name__ == "__main__":
+    main()
